19a8f7d0737772db693da421e336a9dcfd877f56
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-01-13 09:27:40 +0600

    some correction of climbing staris function

--- Fibonacci style/climbingStairs.js
+++ Fibonacci style/climbingStairs.js
@@ -1,6 +1,17 @@
 
 const climbStairs = (n) => {
-    return n
+    if(n <= 1){
+        return 1
+    }
+   let firstStep = 1;
+   let secondStep = 1;
+   let thirdStep = 0;
+
+   for(let i = 2; i <= n; i++ ){
+       thirdStep = firstStep + secondStep;
+       firstStep = secondStep;
+       secondStep = thirdStep;
+   }
+   return thirdStep
 };
 
-console.log(climbStairs(5))
