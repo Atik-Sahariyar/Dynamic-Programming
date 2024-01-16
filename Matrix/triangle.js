80bfa3bccbdf7e38db02b18d3c0f51cdf69c5477
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-01-16 09:43:23 +0600

    added minimumTotal function for calculation of mminimmum paths summation

---
+++ Matrix/triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function (triangle) {
+    const dpTriangle = [...triangle];
+
+    for (let i = dpTriangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < dpTriangle[i].length; j++) {
+            dpTriangle[i][j] += Math.min(dpTriangle[i + 1][j], dpTriangle[i + 1][j + 1]);
+        }
+    }
+
+    return dpTriangle[0][0];
+};
+
